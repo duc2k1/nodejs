be764e02e2be47810121affabe3a7575d24c0602
duc2k1 <duc.phan622@gmail.com>
2022-04-22 11:36:24 +0700

    Init

---
+++ dist/app.js
@@ -0,0 +1,21 @@
+"use strict";
+function calculateNumber(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+const numberOne = 10;
+const numberTwo = 20;
+console.log(calculateNumber(numberOne, numberTwo));
+var Gender;
+(function (Gender) {
+    Gender["MALE"] = "MALE";
+    Gender["FEMALE"] = "FEMALE";
+})(Gender || (Gender = {}));
+const myName = "PhanTaiDuc";
+const myAge = 1000;
+const myHeight = 1.75;
+const myWeight = 70;
+const myGender = Gender.FEMALE;
+const student = {
+    name: "Michanel",
+    height: 100,
+};
---
+++ src/app.ts
@@ -0,0 +1,34 @@
+function calculateNumber(numberOne: number, numberTwo: number): number {
+  return numberOne + numberTwo;
+}
+
+const numberOne: number = 10;
+const numberTwo: number = 20;
+
+console.log(calculateNumber(numberOne, numberTwo));
+
+enum Gender {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+}
+
+type People = {
+  height?: number;
+  weight?: number;
+};
+
+type Student = People & {
+  name?: string;
+  age?: number;
+  sex?: Gender;
+};
+
+const myName: string = "PhanTaiDuc";
+const myAge: number = 1000;
+const myHeight: number = 1.75;
+const myWeight: number = 70;
+const myGender: Gender = Gender.FEMALE;
+const student: Student = {
+  name: "Michanel",
+  height: 100,
+};
